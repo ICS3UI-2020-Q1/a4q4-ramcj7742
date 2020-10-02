6c0d6747e70448213178288c706681d3e8228632
ramcj7742 <ramcj7742@wrdsb.ca>
2020-10-02 16:03:20 +0000

    Finished Fizzbuzz

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ * Fizzbuzz until user number
+ * @author Jaden Ramcharan
  */
 public class Main {
 
@@ -9,7 +10,35 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+
+    
+    //Had to redo because I forgot to save before
+
+
+    // Creates scanner for user input
+    Scanner input = new Scanner(System.in);
+
+    //Gets number from user
+    System.out.println("Please enter a positive integer to see the FizzBuzz numbers");
+    int num = input.nextInt();
+
+    //start, fizz, and buzz variables
+    int START = 0;
+    int fizz = 3;
+    int buzz = 5;
+
+    //Fizzbuzz loop
+    while(num > START){
+      START = START + 1;
+      if(START % fizz == 0 && START % buzz == 0){
+        System.out.println("Fizzbuzz");
+      }else if(START % fizz == 0){
+        System.out.println("Fizz");
+      }else if(START % buzz ==0){
+        System.out.println("Buzz");
+      }else
+      System.out.println(START);
+    }
     
   }
 }
